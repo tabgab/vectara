6a8256340c23ed447145b9ec4a61d317552143fc
tabgab <gabor.tabi@gmail.com>
2023-08-14 14:52:12 +0200

    Cosmetics with error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -231,6 +231,7 @@ if len(OPENAI_API_KEY)>5:
         answer = response.choices[0].text.strip()
         st.text_area("Answer:", value=answer, height=600)
       else:
+        st.error('Too many tokens submitted error!', icon="🚨")
         st.text_area("I am sorry, your query exceeds the model's capabilities. The maximum tokens must be 4097. You submitted: "+str(numtokens)+
                      " Please change the question to reduce this.")
 
