a606d2a752a30f3fe6b270dfe5ac680159f78cf2
tabgab <gabor.tabi@gmail.com>
2023-08-14 14:54:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -231,7 +231,8 @@ if len(OPENAI_API_KEY)>5:
         answer = response.choices[0].text.strip()
         st.text_area("Answer:", value=answer, height=600)
       else:
-        st.error('Too many tokens submitted error!', icon="🚨")
+        st.error("Too many tokens submitted error! I am sorry, your query exceeds the model's capabilities. The maximum tokens must be 4097. You submitted: "+str(numtokens)+
+                     " Please change the question to reduce this.', icon="🚨")
         st.text_area("I am sorry, your query exceeds the model's capabilities. The maximum tokens must be 4097. You submitted: "+str(numtokens)+
                      " Please change the question to reduce this.")
 
